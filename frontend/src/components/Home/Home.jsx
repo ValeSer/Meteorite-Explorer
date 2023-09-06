d6312eeee334027adc9902abaf732856ec69137f
Valeria Serci <balerty84@gmail.com>
2023-09-06 17:19:29 +0100

    display first elname

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,31 @@
 import './Home.scss';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [meteorites, setMeteorites] = useState('');
+
+  useEffect(() => {
+      axios.get('http://localhost:8080')
+        .then((response) => {
+          //if(response.status !== 200)throw new Error('Failed to fetch');
+          setMeteorites(response.data[0].name);
+          console.log('data from API', response.data)
+        })
+        .catch(error => {
+          console.error('error', error);
+        });
+  }, []);
+   
+
   return (
     <>
-      Hello from Home
+      <div>
+        {meteorites}
+      </div>
+      
     </>
   )
 }
 
-export default Home ;
+export default Home;
