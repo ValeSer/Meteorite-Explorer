9351b54adf305189bd8f07c1304d4b9340aba738
Valeria Serci <balerty84@gmail.com>
2023-09-06 21:43:15 +0100

    render list of meteorites

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,13 +3,30 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Home = () => {
-  const [meteorites, setMeteorites] = useState('');
+  const [meteorites, setMeteorites] = useState([]);
+
+  const renderMeteorites = () => {
+    return (
+      <div className='met-table-container'>
+        {
+          meteorites.map((met) => {
+            return (
+              <div key={met.id}>
+                {met.name}
+              </div>
+            
+            )
+          })
+        }
+      </div>
+    )
+  }
 
   useEffect(() => {
       axios.get('http://localhost:8080')
         .then((response) => {
           //if(response.status !== 200)throw new Error('Failed to fetch');
-          setMeteorites(response.data[0].name);
+          setMeteorites(response.data);
           console.log('data from API', response.data)
         })
         .catch(error => {
@@ -21,7 +38,7 @@ const Home = () => {
   return (
     <>
       <div>
-        {meteorites}
+        {renderMeteorites()}
       </div>
       
     </>
