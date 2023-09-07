eb1a31ffb144e80583aaa4d00f54e29d959e63ca
Valeria Serci <balerty84@gmail.com>
2023-09-07 16:06:38 +0100

    display more meteorites data

--- frontend/src/components/Meteorite/Meteorite.jsx
+++ frontend/src/components/Meteorite/Meteorite.jsx
@@ -4,8 +4,15 @@ const Meteorite = (props) => {
 
   return (
     <>
-      <div>{props.data.id}</div>
       <div>{props.data.name}</div>
+      <div>{props.data.id}</div>
+      <div>{props.data.nametype}</div>
+      <div>{props.data.mass}</div>
+      <div>{props.data.fall}</div>
+      <div>{props.data.recclass}</div>
+      <div>{props.data.reclat}</div>
+      <div>{props.data.reclong}</div>
+      <div>{props.data.year}</div>
     </>
   )
 }
