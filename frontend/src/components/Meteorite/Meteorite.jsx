f10040f0d649d92d6b0f95b214988d1360ce2717
Valeria Serci <balerty84@gmail.com>
2023-09-07 17:04:57 +0100

    give classNames to each row

--- frontend/src/components/Meteorite/Meteorite.jsx
+++ frontend/src/components/Meteorite/Meteorite.jsx
@@ -3,17 +3,17 @@ import './Meteorite.scss';
 const Meteorite = (props) => {
 
   return (
-    <>
-      <div>{props.data.name}</div>
-      <div>{props.data.id}</div>
-      <div>{props.data.nametype}</div>
-      <div>{props.data.mass}</div>
-      <div>{props.data.fall}</div>
-      <div>{props.data.recclass}</div>
-      <div>{props.data.reclat}</div>
-      <div>{props.data.reclong}</div>
-      <div>{props.data.year}</div>
-    </>
+    <div className='row'>
+      <div className='name'>{props.data.name}</div>
+      <div className='id'>{props.data.id}</div>
+      <div className='nametype'>{props.data.nametype}</div>
+      <div className='mass'>{props.data.mass}</div>
+      <div className='fall'>{props.data.fall}</div>
+      <div className='recclass'>{props.data.recclass}</div>
+      <div className='reclat'>{props.data.reclat}</div>
+      <div className='reclong'>{props.data.reclong}</div>
+      <div className='year'>{props.data.year}</div>
+    </div>
   )
 }
 
