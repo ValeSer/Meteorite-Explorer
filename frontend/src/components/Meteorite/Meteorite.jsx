8a20095e1d55304fba49e8f9cac7578ca2f33fca
Valeria Serci <balerty84@gmail.com>
2023-09-08 17:58:48 +0100

    add a function to format date

--- frontend/src/components/Meteorite/Meteorite.jsx
+++ frontend/src/components/Meteorite/Meteorite.jsx
@@ -1,6 +1,19 @@
 import './Meteorite.scss';
 
 const Meteorite = (props) => {
+  const extractYear = (dateString) => {
+    try {
+      const date = new Date(dateString);
+      if (!isNaN(date)) {
+        return date.getFullYear().toString();
+      }
+    } catch (error) {
+      console.error('Error parsing date:', error);
+    }
+    return 'Invalid Date';
+  };
+
+  const year = extractYear(props.data.year);
 
   return (
     <div className='row'>
@@ -12,7 +25,7 @@ const Meteorite = (props) => {
       <div className='recclass'>{props.data.recclass}</div>
       <div className='reclat'>{props.data.reclat}</div>
       <div className='reclong'>{props.data.reclong}</div>
-      <div className='year'>{props.data.year}</div>
+      <div className='year'>{year}</div>
     </div>
   )
 }
