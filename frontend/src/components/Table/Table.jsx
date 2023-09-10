ca984e815d325baab3cb4722437841ffe5ab3f4d
Valeria Serci <balerty84@gmail.com>
2023-09-10 19:23:52 +0100

    add new component and move the logic from the Home component

---
+++ frontend/src/components/Table/Table.jsx
@@ -0,0 +1,34 @@
+import './Table.scss';
+import Meteorite from '../Meteorite/Meteorite'
+
+const Table = (props) => {
+   
+  const renderMeteorites = () => {
+  
+    return (
+      <div className='met-table-container'>
+        {
+          props.meteorites.map((met) => {
+            return (
+              <Meteorite 
+                data={met}
+                key={met.id}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
+  return (
+    <div>
+        {renderMeteorites()}
+    </div>
+  )
+  
+}
+
+export default Table;
+
+
+
