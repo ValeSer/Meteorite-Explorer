104dab1633d480d1bcd525b9d92b00975a396e25
Valeria Serci <balerty84@gmail.com>
2023-09-10 19:59:47 +0100

    search button displayed

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -3,8 +3,9 @@ import './Search.scss';
 const Search = () => {
 
   return (
-    <div>
-      Hello from Search
+    <div className='search-container'>
+      <input placeholder='Enter search terms'></input>
+      <button>SEARCH</button>
     </div>
   )
 }
