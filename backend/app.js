54889af6d298983bdbb55a98d7b97fdb20415c3f
Valeria Serci <balerty84@gmail.com>
2023-09-13 17:18:40 +0100

    write query string with the user input and case insensitive

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.get("/", (req, res) => {
-  axios.get('https://data.nasa.gov/resource/gh4g-9sfh.json?$order=name')
+  axios.get(`https://data.nasa.gov/resource/gh4g-9sfh.json?$where=lower(name) like lower('%25${req.query.searchTerm}%25')`)
     .then((response) => {
       res.send(response.data);
     })
