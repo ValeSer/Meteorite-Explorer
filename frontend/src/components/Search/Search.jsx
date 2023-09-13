5f776f9d7138f8b8e1ccb320ba7856725795c019
Valeria Serci <balerty84@gmail.com>
2023-09-13 17:21:10 +0100

    change name of the param from query to searchTerm

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -2,17 +2,17 @@ import './Search.scss';
 import { useState } from 'react';
 
 const Search = (props) => {
-  const [query, setQuery] = useState('');
+  const [searchTerm, setsearchTerm] = useState('');
 
   const searchMeteorites = () => {
-    props.searchMeteorites(query);
+    props.searchMeteorites(searchTerm);
   }
 
   return (
     <div className='search-container'>
       <input placeholder='Enter search terms'
-        onChange={e => setQuery(e.target.value)}
-        value={query}
+        onChange={e => setsearchTerm(e.target.value)}
+        value={searchTerm}
       />
       <button onClick={searchMeteorites} >SEARCH</button>
     </div>
