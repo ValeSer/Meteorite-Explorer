1669536b85b204c1dadd52358b70c16059d9d952
Valeria Serci <balerty84@gmail.com>
2023-09-16 11:36:26 +0100

    fix order of elements in the meteorite components

--- frontend/src/components/Meteorite/Meteorite.jsx
+++ frontend/src/components/Meteorite/Meteorite.jsx
@@ -20,12 +20,12 @@ const Meteorite = (props) => {
       <div className='name'>{props.data.name}</div>
       <div className='id'>{props.data.id}</div>
       <div className='nametype'>{props.data.nametype}</div>
+      <div className='recclass'>{props.data.recclass}</div>
       <div className='mass'>{props.data.mass}</div>
       <div className='fall'>{props.data.fall}</div>
-      <div className='recclass'>{props.data.recclass}</div>
+      <div className='year'>{year}</div>
       <div className='reclat'>{props.data.reclat}</div>
       <div className='reclong'>{props.data.reclong}</div>
-      <div className='year'>{year}</div>
     </div>
   )
 }
