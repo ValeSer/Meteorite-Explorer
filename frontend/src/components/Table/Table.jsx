8820af49fa620e6ea2fa50ccddb1bd8f1ab49df2
Valeria Serci <balerty84@gmail.com>
2023-09-18 16:31:29 +0100

    fix style for mass class

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -8,13 +8,13 @@ const Table = (props) => {
     return (
       <div className='met-table-container'>
         <div className='table-header row'>
-        <p>Name</p>
+        <p className='name'>Name</p>
         <p>Id</p>
         <p>Name Type</p>
         <p>Rec Class</p>
-        <p>Mass(g)</p>
+        <p className='mass'>Mass(g)</p>
         <p>Fall</p>
-        <p>Year</p>
+        <p className='year'>Year</p>
         <p>Latitude</p>
         <p>Longitude</p>
       </div>
