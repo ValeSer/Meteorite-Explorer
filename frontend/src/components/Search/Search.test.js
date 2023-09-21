42461ba70213509950a14ef9dafd110d9ec1b99b
Valeria Serci <balerty84@gmail.com>
2023-09-21 22:27:55 +0100

    test returns input passing

--- frontend/src/components/Search/Search.test.js
+++ frontend/src/components/Search/Search.test.js
@@ -1,10 +1,10 @@
-import { getAllByPlaceholderText, render } from '@testing-library/react'
+import { fireEvent, getAllByPlaceholderText, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import Search from './Search'
 import React from 'react';
 
-it('renders empty string with no input', (done) => {
+it('returns empty string with no input', (done) => {
   const user = userEvent.setup()
   const searchMeteorites = (searchTerm) => {
     expect(searchTerm).toEqual('');
@@ -16,7 +16,24 @@ it('renders empty string with no input', (done) => {
   const input = getByPlaceholderText('Enter search terms');
 
   user.click(button)
-  
+});
 
-  
+it('returns input', (done) => {
+  const user = userEvent.setup()
+  const searchMeteorites = (searchTerm) => {
+    expect(searchTerm).toEqual('Aac');
+    done();
+  }
+
+  const { getByRole, getByPlaceholderText } = render(<Search searchMeteorites={searchMeteorites}/>);
+  const button =  getByRole('button');
+  const input = getByPlaceholderText('Enter search terms');
+  fireEvent.change(input, {
+    target: {
+      value: 'Aac'
+    }
+  })
+
+  user.click(button)
+    
 });
