cc4177071461782f0aeb21800cc6da8e18882865
Valeria Serci <balerty84@gmail.com>
2023-09-24 23:02:12 +0100

    add the CircleLoader

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Table from '../Table/Table';
 import Search from '../Search/Search';
+import CircleLoader from "react-spinners/ClipLoader";
 
 const Home = () => {
   const [meteorites, setMeteorites] = useState([]);
@@ -36,6 +37,13 @@ const Home = () => {
         <Search searchMeteorites={searchMeteorites}/>
         <Table meteorites={meteorites}/>
       </div>
+      <CircleLoader
+          color={'#ffffff'}
+          loading={loading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+      />
     </>
   )
 }
