6e8d8b646677f4ae5942f70c1eb0fc6292fe4b09
Valeria Serci <balerty84@gmail.com>
2023-09-25 16:27:00 +0100

    label for each row

--- frontend/src/components/Meteorite/Meteorite.jsx
+++ frontend/src/components/Meteorite/Meteorite.jsx
@@ -26,13 +26,34 @@ const Meteorite = (props) => {
         <div className='label'>Id:</div>
         <div className='element'>{props.data.id}</div>
       </div>
-      <div className='nametype'>{props.data.nametype}</div>
-      <div className='recclass'>{props.data.recclass}</div>
-      <div className='mass'>{props.data.mass}</div>
-      <div className='fall'>{props.data.fall}</div>
-      <div className='year'>{year}</div>
-      <div className='reclat'>{props.data.reclat}</div>
-      <div className='reclong'>{props.data.reclong}</div>
+      <div className=' cell nametype'>
+        <div className='label'>Name Type:</div>
+        <div className='element'>{props.data.nametype}</div>
+      </div>
+      <div className='cell recclass'>
+        <div className='label'>Rec Class:</div>
+        <div className='element'>{props.data.recclass}</div>
+      </div>
+      <div className='cell mass'>
+        <div className='label'>Mass(g):</div>
+        <div className='element'>{props.data.mass}</div>
+      </div>
+      <div className='cell fall'>
+        <div className='label'>Fall:</div>
+        <div className='element'>{props.data.fall}</div>
+      </div>
+      <div className='cell year'>
+        <div className='label'>Year:</div>
+        <div className='element'>{year}</div>
+      </div>
+      <div className='cell reclat'>
+        <div className='label'>Latitude:</div>
+        <div className='element'>{props.data.reclat}</div>
+      </div>
+      <div className='cell reclong'>
+        <div className='label'>Longitude:</div>
+        <div className='element'>{props.data.reclong}</div>
+      </div>
     </div>
   )
 }
